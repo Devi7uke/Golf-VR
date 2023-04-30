d51edabbb16065e0d1b25a2d34e4efd61e61353e
Devi7uke <dvlk8003@gmail.com>
2023-04-30 16:53:51 -0600

    Golf Club Physics
    The implementation of the force vector for the collision between the golf ball and club has been completed.

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -1,22 +1,66 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
-
+[RequireComponent (typeof (Rigidbody))]
+[RequireComponent (typeof(LineRenderer))]
 public class Ball : MonoBehaviour
 {
+    [SerializeField]
+    private float MaxForce;
+    [SerializeField]
+    private float forceModifier = 0.5f;
+    [SerializeField]
+    private float force;
     private Rigidbody rb;
+    private LineRenderer lineRenderer;
+    private Vector3 start, end;
+    private bool allowShoot = false;
+    private Vector3 direction;
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        lineRenderer = GetComponent<LineRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space)){
-            rb.AddForce(Vector3.forward, ForceMode.Impulse);
-            Debug.Log("Force added!!!");
+        if (rb.velocity == Vector3.zero)
+        {
+            rb.angularVelocity = Vector3.zero;
+        }
+    }
+
+    //Hereeeeeeeeeeeeeeeeeeeeeeeeeeee
+    private void ResetPosition()
+    {
+
+    }
+    private void NextLevel()
+    {
+
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.name == "Limits") {
+            ResetPosition();
+        }
+        else if (other.name == "Hole")
+        {
+            NextLevel();
+        }
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Club" && allowShoot)
+        {
+            allowShoot = false;
+            direction = start - end;
+            rb.AddForce(direction * force, ForceMode.Impulse);
+            force = 0;
+            start = end = Vector3.zero;
         }
     }
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -11,78 +11,116 @@ public class Player : MonoBehaviour
 	private Rigidbody ballRB;
 	[SerializeField]
 	private float force = 10.0f;
+	private bool allowStroke = true;
+	private Vector3 starPosition;
+	private Quaternion prevRotation;
+	private float angularSpeed;
 	// Start is called before the first frame update
 	void Start()
 	{
 		rb = GetComponent<Rigidbody>();
 		ballRB = ball.GetComponent<Rigidbody>();
+		starPosition = ball.transform.position;
+		prevRotation = transform.rotation;
+
 	}
 
 	// Update is called once per frame
 	void Update()
 	{
-		
-	}
-    Vector3 GetClubFaceNormal(GameObject club, Vector3 pointOfImpact)
-    {
-        Mesh clubMesh = club.GetComponent<MeshFilter>().mesh;
-        int[] triangles = clubMesh.triangles;
-        Vector3 closestFaceNormal = Vector3.zero;
-        float closestDistance = float.MaxValue;
 
-        // Iterate over each triangle face in the mesh
-        for (int i = 0; i < triangles.Length; i += 3)
-        {
-            Vector3 v1 = clubMesh.vertices[triangles[i]];
-            Vector3 v2 = clubMesh.vertices[triangles[i + 1]];
-            Vector3 v3 = clubMesh.vertices[triangles[i + 2]];
-
-            // Calculate the normal of the face
-            Vector3 faceNormal = Vector3.Cross(v2 - v1, v3 - v1).normalized;
-
-            // Calculate the distance from the face to the point of impact
-            float distance = Vector3.Distance(pointOfImpact, v1);
-
-            // If this face is closer to the point of impact than the previous closest face, set it as the new closest face
-            if (distance < closestDistance)
-            {
-                closestDistance = distance;
-                closestFaceNormal = faceNormal;
-            }
-        }
-        return closestFaceNormal;
-    }
-    float CalculateForceMagnitude(float angle)
-    {
-        // Clamp the angle to a maximum of 45 degrees
-        angle = Mathf.Clamp(angle, 0.0f, 45.0f);
-
-        // Map the angle to a force magnitude between 0.0 and 1.0
-        float normalizedAngle = angle / 45.0f;
-        float forceMagnitude = Mathf.Lerp(0.0f, 1.0f, normalizedAngle);
-
-        // Scale the force magnitude by the desired maximum force
-        forceMagnitude *= force;
-
-        return forceMagnitude;
-    }
-
-    private void OnCollisionEnter(Collision collision)
-    {
-        if (collision.gameObject.tag == "Ball")
+		if (ball.GetComponent<Rigidbody>().velocity == Vector3.zero)
 		{
+			allowStroke = true;
+			ball.GetComponent<Rigidbody>().angularVelocity = Vector3.zero;
+			gameObject.GetComponent<Collider>().enabled = true;
+		}
+		if (Input.GetKeyDown(KeyCode.Space))
+		{
+			RestartBallPosition();
+		}
+        angularSpeed = AngularRotationSpeed();
+    }
+	private float AngularRotationSpeed()
+	{
+        Quaternion rotationChange = transform.rotation * Quaternion.Inverse(prevRotation);
+
+        // Calculate the angular velocity based on the rotation change
+        float angleInDegrees;
+        Vector3 rotationAxis;
+        rotationChange.ToAngleAxis(out angleInDegrees, out rotationAxis);
+        float angularSpeed = angleInDegrees / Time.deltaTime;
+
+        // Update the previous rotation for the next frame
+        prevRotation = transform.rotation;
+
+        // Output the calculated angular velocity
+        Debug.Log("Angular velocity: " + angularSpeed);
+		return angularSpeed;
+    }
+	Vector3 GetClubFaceNormal(GameObject club, Vector3 pointOfImpact)
+	{
+		Mesh clubMesh = club.GetComponent<MeshFilter>().mesh;
+		int[] triangles = clubMesh.triangles;
+		Vector3 closestFaceNormal = Vector3.zero;
+		float closestDistance = float.MaxValue;
+
+		// Iterate over each triangle face in the mesh
+		for (int i = 0; i < triangles.Length; i += 3)
+		{
+			Vector3 v1 = clubMesh.vertices[triangles[i]];
+			Vector3 v2 = clubMesh.vertices[triangles[i + 1]];
+			Vector3 v3 = clubMesh.vertices[triangles[i + 2]];
+
+			// Calculate the normal of the face
+			Vector3 faceNormal = Vector3.Cross(v2 - v1, v3 - v1).normalized;
+
+			// Calculate the distance from the face to the point of impact
+			float distance = Vector3.Distance(pointOfImpact, v1);
+
+			// If this face is closer to the point of impact than the previous closest face, set it as the new closest face
+			if (distance < closestDistance)
+			{
+				closestDistance = distance;
+				closestFaceNormal = faceNormal;
+			}
+		}
+		return closestFaceNormal;
+	}
+	private void RestartBallPosition()
+	{
+		ball.transform.position = starPosition;
+	}
+    float CalculateForceMagnitude(float angle)
+	{
+		// Clamp the angle to a maximum of 45 degrees
+		angle = Mathf.Clamp(angle, 0.0f, 45.0f);
+
+		// Map the angle to a force magnitude between 0.0 and 1.0
+		float normalizedAngle = angle / 45.0f;
+		float forceMagnitude = Mathf.Lerp(0.0f, 1.0f, normalizedAngle);
+
+		// Scale the force magnitude by the desired maximum force
+		forceMagnitude *= force;
+
+		return forceMagnitude;
+	}
+
+	private void OnCollisionEnter(Collision collision)
+	{
+		if (collision.gameObject.tag == "Ball" && allowStroke)
+		{
+			allowStroke = false;
 			Vector3 forceDirection = ball.transform.position - transform.position;
 			forceDirection = forceDirection.normalized;
-            Vector3 clubFaceNormal = GetClubFaceNormal(gameObject, collision.contacts[0].point);
-            float angle = Vector3.Angle(clubFaceNormal, -forceDirection);
-            float forceMangnitude = CalculateForceMagnitude(angle);
-            Vector3 forceVector = forceMangnitude * clubFaceNormal;
-            Quaternion rotation = Quaternion.Euler(0, 90, 0);
-            Vector3 rotatedForceVector = rotation * forceVector;
-            ballRB.AddForce(rotatedForceVector, ForceMode.Impulse);
-            Debug.Log("Collision Entered!!");
-
-            Debug.DrawRay(ball.transform.position, rotatedForceVector, Color.red, 1.0f);
-        }
-    }
+			Vector3 clubFaceNormal = GetClubFaceNormal(gameObject, collision.contacts[0].point);
+			float angle = Vector3.Angle(clubFaceNormal, -forceDirection);
+			float forceMangnitude = CalculateForceMagnitude(angle);
+			Vector3 rotatedForceVector = Quaternion.Euler(0, 90, 0) * (forceMangnitude * clubFaceNormal);
+			gameObject.GetComponent<Collider>().enabled = false;
+			Vector3 forceVector = rotatedForceVector * Mathf.Abs((angularSpeed > 2) ? angularSpeed / 10 : 2);
+            ballRB.AddForce(forceVector, ForceMode.Acceleration);
+            Debug.DrawRay(ball.transform.position, forceVector, Color.cyan, 1.0f);
+		}
+	}
 }
