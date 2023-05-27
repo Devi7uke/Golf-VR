08eeda2b5f3da37c9c08366cb06325a52d415887
LeonardoMata <leonardomatahernandez98@gmail.com>
2023-05-27 13:09:28 -0600

    JumpScare

---
+++ Assets/Scripts 1/EntrarCasa.cs
@@ -0,0 +1,75 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EntrarCasa : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private IEnumerator coroutine;
+    public GameObject xr_p;
+    public GameObject penta;
+    ParticleSystem portal;
+    public GameObject video;
+
+    void Start()
+    {
+        portal = penta.GetComponent<ParticleSystem>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private IEnumerator MoverJugador(float waitTime)
+    {
+        Debug.Log("MoviendoJugador");
+        yield return new  WaitForSeconds(waitTime);
+        xr_p.transform.position = new Vector3(-0.5f, 5.0f, -23.0f);
+        xr_p.transform.rotation = Quaternion.Euler(0, 270, 0);
+        Debug.Log("MoviendoPelota");
+        yield return new  WaitForSeconds(3.0f);
+        transform.position = new Vector3(-2.75f, 6.0f, -24.0f);
+        portal.Play();
+        yield return new  WaitForSeconds(5.0f);
+        portal.Stop();
+    }
+
+    private IEnumerator Jumpscare(float waitTime)
+    {
+
+        yield return new  WaitForSeconds(3.0f);
+        video.SetActive(true);
+        xr_p.transform.position = new Vector3(-0.5f, 5.0f, -23.0f);
+        xr_p.transform.rotation = Quaternion.Euler(0, 270, 0);
+        transform.position = new Vector3(-1.31f, 4.17f, -23.18f);
+    }
+
+    private IEnumerator OutOfBounds(float waitTime)
+    {
+        yield return new  WaitForSeconds(2.0f);
+        xr_p.transform.position = new Vector3(-0.5f, 5.0f, -23.0f);
+        xr_p.transform.rotation = Quaternion.Euler(0, 270, 0);
+        gameObject.transform.position = new Vector3(-1.31f, 4.17f, -23.18f);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("MoviendoJugador");
+        if (other.tag == "Fuente"){
+            Debug.Log("Salto");
+            coroutine = MoverJugador(2.0f);
+            StartCoroutine(coroutine);
+        }else if(other.tag == "Jumpscare"){
+            Debug.Log("Susto");
+            coroutine = Jumpscare(2.0f);
+            StartCoroutine(coroutine);
+        }else if(other.tag == "Piso"){
+            Debug.Log("Salio");
+            coroutine = OutOfBounds(2.0f);
+            StartCoroutine(coroutine);
+        }
+
+    }
+}
